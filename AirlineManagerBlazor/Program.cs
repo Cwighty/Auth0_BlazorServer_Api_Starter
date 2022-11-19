fe20adc7ce10e002c3c05bdc296718ef63139222
Caleb Wightman <agentcboy@gmail.com>
2022-11-19 12:59:26 -0700

    fixed name not showing up

--- AirlineManagerBlazor/Program.cs
+++ AirlineManagerBlazor/Program.cs
@@ -91,7 +91,8 @@ builder.Services.AddAuthentication(options =>
         options.TokenValidationParameters = new TokenValidationParameters
         {
             // map the claimsPrincipal's roles to the roles claim
-            RoleClaimType = "http://schemas.microsoft.com/ws/2008/06/identity/claims/roles"
+            RoleClaimType = "http://schemas.microsoft.com/ws/2008/06/identity/claims/roles",
+            NameClaimType = "name"
         };
     }
 );
